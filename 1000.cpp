436908c0334c23e9bd5d8f3719fb5228da755824
sunrui <sunrui_2015@outlook.com>
2022-03-29 15:52:28 +0800

    Problem 1000 Accepted

---
+++ 1000.cpp
@@ -0,0 +1,14 @@
+/*
+*	Problem 1000
+*	http://poj.org/problem?id=1000
+*	Date 3/29/2022
+* 	Accepted
+*/
+#include <cstdio>
+
+int main() {
+	int a, b;
+	while(scanf("%d%d", &a, &b) != EOF)
+		printf("%d\n", a + b);
+	return 0;
+}
